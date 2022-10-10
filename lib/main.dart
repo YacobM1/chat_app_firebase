5d5d20d8e09ec3004fe25b73024e4bbc7443a73f
TrueLove <kulakevichyashca@gmail.com>
2022-10-11 01:11:07 +0300

    connected firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_chat/screens/registration_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+
+Future<void> main() async {
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
